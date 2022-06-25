78dbd36db19c2d250ce5125cfa6b9c0b948885b7
suico.dave@gmail.com <suico.dave@gmail.com>
2022-06-25 18:08:48 +0800

    added blazor

--- Client/Program.cs
+++ Client/Program.cs
@@ -4,10 +4,13 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.AspNetCore.Components.Authorization;
 using Client.Auth;
 using Client;
+using MudBlazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddMudServices();
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri($"{builder.HostEnvironment.BaseAddress}api/") });
 
