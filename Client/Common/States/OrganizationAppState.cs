4b1b06fbfd10b1a56cea7949dcd412213f3325c7
suico.dave@gmail.com <suico.dave@gmail.com>
2022-07-21 20:25:46 +0800

    fixed #7

--- Client/Common/States/OrganizationAppState.cs
+++ Client/Common/States/OrganizationAppState.cs
@@ -1,11 +1,21 @@
+using System.Net.Http.Json;
+
 using Microsoft.AspNetCore.Components;
 
+using Shared.Organizations;
 using Shared.Users;
 
 namespace Client.Common.States
 {
     public class OrganizationAppState
     {
+        private readonly HttpClient _http;
+
+        public OrganizationAppState(HttpClient http)
+        {
+            _http = http;
+        }
+
         public IEnumerable<UserOrganizationResponse>? Organizations { get; private set; } = null;
 
         public UserOrganizationResponse? DefaultOrganization => Organizations?.Where(x => x.IsDefault).FirstOrDefault();
@@ -14,13 +24,63 @@ namespace Client.Common.States
         {
             this.Organizations = organizations;
 
-            NotifyStateChanged(source, organizations);
+            NotifyStateChanged();
 
         }
 
-        public event Action<ComponentBase, IEnumerable<UserOrganizationResponse>>? StateChanged;
+        public event Action? StateChanged;
 
-        public void NotifyStateChanged(ComponentBase source, IEnumerable<UserOrganizationResponse> organizations) => StateChanged?.Invoke(source, organizations);
+        public void NotifyStateChanged() => StateChanged?.Invoke();
+
+
+        public async Task GetData()
+        {
+            Organizations = await _http.GetFromJsonAsync<IEnumerable<UserOrganizationResponse>>("Organizations/Owned") ??
+            Enumerable.Empty<UserOrganizationResponse>();
+
+        }
+
+        public async Task LoadWhenEmpty()
+        {
+            if (Organizations is null)
+            {
+                await GetData();
+            }
+        }
+
+        public async Task<HttpResponseMessage> SetAsDefault(int id)
+        {
+            string path = $"Organizations/{id}/SetDefault";
+
+            var response = await _http.PutAsync(path, null);
+
+            if (response.IsSuccessStatusCode)
+            {
+                UserOrganizationResponse currentDefaultOrg = Organizations!.Where(x => x.IsDefault).First();
+
+                currentDefaultOrg.IsDefault = false;
+
+                UserOrganizationResponse selectedOrg = Organizations!.Where(x => x.Id == id).First();
+
+                selectedOrg.IsDefault = true;
+
+                NotifyStateChanged();
+            }
+
+
+            return response;
+        }
+
+        public async Task<HttpResponseMessage> CreateUserOrganization(OrganizationRequest request){
+            var response = await _http.PostAsJsonAsync("Organizations", request);
+
+            if (response.IsSuccessStatusCode)
+            {
+                await GetData();
+            }
+
+            return response;
+        }
 
     }
 }
