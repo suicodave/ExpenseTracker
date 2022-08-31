a275c9c6f4287420f926d1fd7e740d40507bbadd
suico.dave@gmail.com <suico.dave@gmail.com>
2022-08-31 14:42:02 +0800

    fixed #37

---
+++ Shared/Budgets/BudgetAccountRequest.cs
@@ -0,0 +1,9 @@
+namespace Shared.Budgets
+{
+    public class BudgetAccountRequest
+    {
+        public int AccountTypeId { get; set; }
+
+        public decimal Amount { get; set; }
+    }
+}
--- Shared/Budgets/CreateBudgetHeaderRequest.cs
+++ Shared/Budgets/CreateBudgetHeaderRequest.cs
@@ -5,5 +5,7 @@ namespace Shared.Budgets
         public DateTime? CoveredFrom { get; set; } = new DateTime(DateTime.Now.Year, DateTime.Now.Month, 1);
 
         public DateTime? CoveredTo { get; set; } = new DateTime(DateTime.Now.Year, DateTime.Now.Month, 1).AddMonths(1).AddDays(-1);
+
+        public IList<BudgetAccountRequest> BudgetAccounts { get; set; } = new List<BudgetAccountRequest>();
     }
 }
